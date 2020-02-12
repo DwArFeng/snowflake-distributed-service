d8396e556512d326eaca941cecd6eaf2713f0221
DwArFeng <915724865@qq.com>
2020-02-12 22:45:56 +0800

    ExceptionCode偏移量可配置化
    Signed-off-by: DwArFeng <915724865@qq.com>

---
+++ snowflake-distributed-service-node/src/main/java/com/dwarfeng/sfds/node/configuration/ExceptionCodeOffsetConfiguration.java
@@ -0,0 +1,22 @@
+package com.dwarfeng.sfds.node.configuration;
+
+import com.dwarfeng.sfds.sdk.util.ServiceExceptionCodes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class ExceptionCodeOffsetConfiguration {
+
+    @Value("${snowflake.exception_code_offset}")
+    private int exceptionCodeOffset;
+    @Value("${snowflake.exception_code_offset.subgrade}")
+    private int subgradeExceptionCodeOffset;
+
+    @PostConstruct
+    public void init() {
+        ServiceExceptionCodes.setExceptionCodeOffset(exceptionCodeOffset);
+        com.dwarfeng.subgrade.sdk.exception.ServiceExceptionCodes.setExceptionCodeOffset(subgradeExceptionCodeOffset);
+    }
+}
--- snowflake-distributed-service-sdk/src/main/java/com/dwarfeng/sfds/sdk/util/ServiceExceptionCodes.java
+++ snowflake-distributed-service-sdk/src/main/java/com/dwarfeng/sfds/sdk/util/ServiceExceptionCodes.java
@@ -10,10 +10,30 @@ import com.dwarfeng.subgrade.stack.exception.ServiceException;
  */
 public final class ServiceExceptionCodes {
 
+    private static int EXCEPTION_CODE_OFFSET = 1500;
+
     /**
      * 时钟后移异常。
      */
-    public static final ServiceException.Code CLOCK_MOVED_BACKWARDS = new ServiceException.Code(100, "clock moved backwards");
+    public static final ServiceException.Code CLOCK_MOVED_BACKWARDS = new ServiceException.Code(EXCEPTION_CODE_OFFSET, "clock moved backwards");
+
+    /**
+     * 获取异常代号的偏移量。
+     *
+     * @return 异常代号的偏移量。
+     */
+    public static int getExceptionCodeOffset() {
+        return EXCEPTION_CODE_OFFSET;
+    }
+
+    /**
+     * 设置异常代号的偏移量。
+     *
+     * @param exceptionCodeOffset 指定的异常代号的偏移量。
+     */
+    public static void setExceptionCodeOffset(int exceptionCodeOffset) {
+        EXCEPTION_CODE_OFFSET = exceptionCodeOffset;
+    }
 
     private ServiceExceptionCodes() {
         throw new IllegalStateException("禁止实例化");
