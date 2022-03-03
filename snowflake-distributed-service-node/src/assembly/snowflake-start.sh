3efcc40c85bcfed07bf1de393cb5ba7fee051fa1
DwArFeng <915724865@qq.com>
2022-03-03 08:35:19 +0800

    优化程序的启停脚本
    Signed-off-by: DwArFeng <915724865@qq.com>

--- snowflake-distributed-service-node/src/assembly/snowflake-start.sh
+++ snowflake-distributed-service-node/src/assembly/snowflake-start.sh
@@ -8,9 +8,9 @@ cd $basedir || exit
 # shellcheck disable=SC2154
 # shellcheck disable=SC2086
 nohup /bin/java -classpath "lib/*" \
--Dlog.dir=$logdir \
--Dlog.consoleEncoding=UTF-8 \
--Dlog.fileEncoding=UTF-8 \
-${mainClass} \
->/dev/null 2>&1 &
+  -Dlog.dir="$logdir" \
+  -Dlog.consoleEncoding=UTF-8 \
+  -Dlog.fileEncoding=UTF-8 \
+  ${mainClass} \
+  >/dev/null 2>&1 &
 echo $! >$basedir/snowflake.pid
--- snowflake-distributed-service-node/src/assembly/snowflake-stop.sh
+++ snowflake-distributed-service-node/src/assembly/snowflake-stop.sh
@@ -2,5 +2,5 @@
 # 程序的根目录
 basedir=/usr/local/snowflake
 
-PID=$(cat $basedir/snowflake.pid)
+PID=$(cat "$basedir/snowflake.pid")
 kill "$PID"
